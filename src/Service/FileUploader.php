507241f4d15dc8ac23e273612befa0a232418af5
Wojdylak <karolwojdyla37@gmail.com>
2019-11-30 22:07:35 +0100

    Add fileUploader

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -4,7 +4,56 @@
 namespace App\Service;
 
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class FileUploader
 {
+    private $targetDirectory;
 
+    public function __construct($targetDirectory)
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @return bool|string
+     */
+    public function upload(UploadedFile $file)
+    {
+        $filename = md5(uniqid()) . '.' . $file->guessClientExtension();
+        try {
+            $file->move(
+                $this->getTargetDirectory(),
+                $filename
+            );
+        } catch (FileException $e) {
+            return false;
+        }
+
+        return $filename;
+    }
+
+    /**
+     * @param string $filename
+     */
+    public function remove(string $filename)
+    {
+        if (!is_null($filename)){
+            $filesystem = new Filesystem();
+            $filesystem->remove(
+                $this->getTargetDirectory() . $filename
+            );
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
 }
--- src/Service/PictureService.php
+++ src/Service/PictureService.php
@@ -4,7 +4,125 @@
 namespace App\Service;
 
 
+use App\Entity\Picture;
+use App\Repository\PictureRepository;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class PictureService
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var PictureRepository
+     */
+    private $pictureRepository;
+    /**
+     * @var FileUploader
+     */
+    private $fileUploader;
 
+    public function __construct(EntityManagerInterface $entityManager, PictureRepository $pictureRepository, FileUploader $fileUploader)
+    {
+        $this->entityManager = $entityManager;
+        $this->pictureRepository = $pictureRepository;
+        $this->fileUploader = $fileUploader;
+    }
+
+    /**
+     * @param int $id
+     * @return Picture|null
+     */
+    public function get(int $id)
+    {
+        return $this->pictureRepository->find($id);
+    }
+
+    /**
+     * @param Criteria $criteria
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getList(Criteria $criteria)
+    {
+        return $this->pictureRepository->matching($criteria);
+    }
+
+    /**
+     * @param Picture $picture
+     * @return Picture
+     */
+    public function create(Picture $picture)
+    {
+        $this->entityManager->persist($picture);
+        $this->entityManager->flush();
+
+        return $picture;
+    }
+
+    /**
+     * @param Picture $picture
+     * @param UploadedFile $file
+     * @return Picture
+     */
+    public function update(Picture $picture, UploadedFile $file)
+    {
+        if (false === in_array($picture->getFilename(), ['default_profile.jpg', 'default_notice.jpg'])) {
+            $this->fileUploader->remove($picture->getFilename());
+        }
+        $filename = $this->fileUploader->upload($file);
+
+        $picture->setFilename($filename);
+        $picture->setPath('/uploads/' . $filename);
+
+        $this->entityManager->persist($picture);
+        $this->entityManager->flush();
+
+        return $picture;
+    }
+
+    /**
+     * @param Picture $picture
+     */
+    public function delete(Picture $picture)
+    {
+        $this->entityManager->remove($picture);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param Picture $picture
+     * @return Picture
+     */
+    public function setDefaultProfile(Picture $picture)
+    {
+        $this->fileUploader->remove($picture->getFilename());
+
+        $picture->setFilename('default_profile.jpg');
+        $picture->setPath('/uploads/default_profile.jpg');
+
+        $this->entityManager->persist($picture);
+        $this->entityManager->flush();
+
+        return $picture;
+    }
+
+    /**
+     * @param Picture $picture
+     * @return Picture
+     */
+    public function setDefaultNotice(Picture $picture)
+    {
+        $this->fileUploader->remove($picture->getFilename());
+
+        $picture->setFilename('default_notice.jpg');
+        $picture->setPath('/uploads/default_notice.jpg');
+
+        $this->entityManager->persist($picture);
+        $this->entityManager->flush();
+
+        return $picture;
+    }
 }
