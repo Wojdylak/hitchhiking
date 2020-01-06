c4c71458b7bea50cc54244e30856e924943f1c90
Wojdylak <karolwojdyla37@gmail.com>
2020-01-06 03:18:19 +0100

    fix release

--- heroku-release-tasks.sh
+++ heroku-release-tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 php bin/console doctrine:migrations:migrate -n --allow-no-migration
-PASS="$(printenv JWT_PASSPHRASE)"
-mkdir -p config/jwt
-openssl genpkey -out config/jwt/private.pem -pass $PASS -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
-openssl pkey -in config/jwt/private.pem -passin $PASS -out config/jwt/public.pem -pubout
+#PASS="$(printenv JWT_PASSPHRASE)"
+#mkdir -p config/jwt
+#openssl genpkey -out config/jwt/private.pem -pass $PASS -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+#openssl pkey -in config/jwt/private.pem -passin $PASS -out config/jwt/public.pem -pubout
